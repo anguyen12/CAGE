3577a9c4a7de0941735fd6a5973664ae0362f948
AN <anguyen@macalester.edu>
2015-03-03 16:03:01 -0600

    fixed conflict

--- test_experiment2.js
+++ test_experiment2.js
@@ -3,7 +3,6 @@
 (function() {
   var app = angular.module('tabRunner', []);
   var firstBuild = { tournName: "" , totalTeams: 0, };
-<<<<<<< HEAD
   var teamObjectOLD = {
 	number: 0, //lets make this the one that is assigned by tabber, and uniqueID as the school's special id
 	name: "",
@@ -34,26 +33,6 @@ function teamObject(inputnumber) {
   app.controller('FormController', function($scope){
 	this.isValid = false;
 	
-=======
-
-function teamObject(number) { //sjjasdksajdsaljdla
-	this.number= 0, //lets make this the one that is assigned by tabber, and uniqueID as the school's special id
-	this.name= "",
-	this.record= 0, //ballots won
-	this.pointDiff= 0, //points won
-	this.combinedStr= 0, //combined strength
-	this.rank= 0,
-	this.impermissibles= [], //a list of teams (teamObjects) a team cannot face
-	this.status= "", //plaintiff or defense
-	this.uniqueID= 0
-};
-	
-//http://stackoverflow.com/questions/12928752/validation-of-dynamic-created-form-angularjs not super helpful
-//better???: http://stackoverflow.com/questions/15843765/angularjs-fields-added-dynamically-are-not-registered-on-formcontroller
-  app.controller('FormController', function($scope){
-	this.isValid = false;
-	
->>>>>>> d403e25b370f7208ce96c178b4dd6cd450bf852f
 	this.validateForms = function(){
 		alert("What up");
 		//$scope.testing = "WHATSUP";
@@ -76,7 +55,6 @@ function teamObject(number) { //sjjasdksajdsaljdla
 			document.getElementById('errorMessage').innerText = "Please enter a valid number";
 			return false;
 		}
-<<<<<<< HEAD
 
 		if ((pTourn == "") || (pTotal == "")){
 			document.getElementById('errorMessage').innerText = "Please fill out all fields";
@@ -119,54 +97,11 @@ function teamObject(number) { //sjjasdksajdsaljdla
 	this.test = function(listNumbers){
 		this.listAllTeams = []; //to be filled with teamObjects
 
-=======
-
-		if ((pTourn == "") || (pTotal == "")){
-			document.getElementById('errorMessage').innerText = "Please fill out all fields";
-			return false;
-		}
-		
-		else{
-			document.getElementById('errorMessage').innerText = "";
-			//this.totalTeams = pTotal;
-			this.showAllTeams = true;
-			this.hideSetupForm = true;
-			//create a list of numbers (of the total teams participating)
-			this.buildList(pTotal);
-			//loop through that
-			var finalList = this.test(this.list);
-			return true;
-		}
-	};
-	
-	this.buildList = function(number){
-		for (var i = 0 ; i < number; i++) { 
-			this.list.push(i+1);
-			//var newTeam = new teamObject(i+1);
-			//this.listAllTeams.push(newTeam); //We would hope that by the end of this..
-			//listAllTeams looks like this: [{number: 1}, {number: 2}, {number: 3} ... etc] 
-			//a list of incomplete team info that will be retrieved later..
-		}
-		
-		
-	};
-	
-	$scope.formDataTwo = {};
-	$scope.formDataTwo.list = this.list;
-	
-	//http://stackoverflow.com/questions/12518259/using-ng-repeat-with-table-rows
-	//http://jsfiddle.net/6aqtj/1/
-	
-	this.test = function(listNumbers){
-		this.listAllTeams = []; //to be filled with teamObjects
-
->>>>>>> d403e25b370f7208ce96c178b4dd6cd450bf852f
 		for (var j=0; j < listNumbers.length; j+=2){
 			if (j+1 >= listNumbers.length){
 				this.pairings.push([]);
 				this.pairings[this.pairings.length-1].push(listNumbers[j]);
 				var newTeam = new teamObject(j);
-<<<<<<< HEAD
 				var byeTeam = new teamObject(j+1); //create a bye team when there is odd number of teams - EA
 				byeTeam.byeTeam = true;
 				byeTeam.name = "Bye Team";
@@ -187,22 +122,6 @@ function teamObject(number) { //sjjasdksajdsaljdla
 				newTeam2.status = "d"; 
 				this.listAllTeams.push([newTeam, newTeam2]);
 			}
-=======
-				this.listAllTeams.push(newTeam);
-			}
-			
-			else{
-				this.pairings.push([]);
-				
-				this.pairings[this.pairings.length-1].push(listNumbers[j]);
-				var newTeam = new teamObject(j);
-				this.listAllTeams.push(newTeam);
-				
-				this.pairings[this.pairings.length-1].push(listNumbers[j+1]);
-				var newTeam2 = new teamObject(j+1);
-				this.listAllTeams.push(newTeam2);
-			}
->>>>>>> d403e25b370f7208ce96c178b4dd6cd450bf852f
 		}
 		return this.listAllTeams;
 	}
