d403e25b370f7208ce96c178b4dd6cd450bf852f
caracheng <ccheng@macalester.edu>
2015-03-02 21:38:36 -0600

    Most recently corrected file

--- test_experiment2.js
+++ test_experiment2.js
@@ -1,130 +1,126 @@
-
-/* OVERVIEW
-There is 1 poorly written inefficient controllers.
-
-SetupController
-- validate(name of the tournament, number of teams)
--		this is currently a badly written method because it does more than just validating the form
-- buildList(number of n teams)
--		sets the list instance variable to a list of [1, 2, ... n] (within itself)
-- test(list of n numbers where n is the number of teams in the tournament)
-- 		BAD NAME. should be called something like pairUpTeams. returns a list of lists, i.e. [[1,2],[3,4],[5]]
--		Resets it to finalList. (in the validate function)
-- testR1()
-- 		Testing.
-
-*/
-
+//no nested views: (wait are views == contrllers?) http://jan.varwig.org/archive/how-to-do-nested-views-in-angularjs-hint-dont
+//-------------------------------------------------------------unrelated content exists above---------
 (function() {
-	var app = angular.module('tabRunner', []);
-	var firstSetup = { tournName: "" , totalTeams: 0, };
+  var app = angular.module('tabRunner', []);
+  var firstBuild = { tournName: "" , totalTeams: 0, };
+
+function teamObject(number) { //sjjasdksajdsaljdla
+	this.number= 0, //lets make this the one that is assigned by tabber, and uniqueID as the school's special id
+	this.name= "",
+	this.record= 0, //ballots won
+	this.pointDiff= 0, //points won
+	this.combinedStr= 0, //combined strength
+	this.rank= 0,
+	this.impermissibles= [], //a list of teams (teamObjects) a team cannot face
+	this.status= "", //plaintiff or defense
+	this.uniqueID= 0
+};
 	
-	function teamObject(number) {
-		this.number= number, //lets make this the one that is assigned by tabber, and uniqueID as the school's special id
-		this.name= "",
-		this.record= 0, //ballots won
-		this.pointDiff= 0, //points won
-		this.combinedStr= 0, //combined strength
-		this.rank= 0,
-		this.impermissibles= [], //a list of teams (teamObjects) a team cannot face
-		this.status= "", //plaintiff or defense
-		this.uniqueID= 0
-	}
+//http://stackoverflow.com/questions/12928752/validation-of-dynamic-created-form-angularjs not super helpful
+//better???: http://stackoverflow.com/questions/15843765/angularjs-fields-added-dynamically-are-not-registered-on-formcontroller
+  app.controller('FormController', function($scope){
+	this.isValid = false;
+	
+	this.validateForms = function(){
+		alert("What up");
+		//$scope.testing = "WHATSUP";
+	};
+	
+  });
 
-//SetupController : on the first 'screen' -- figures out what to do and display on the next 'screen' based on form input
-//Poorly written and can be cleaned up.
-	app.controller('SetupController', function($scope){
-		this.newTourn = firstSetup;
-		this.hideSetupForm = false;
-		this.showAllTeams = false;
-		//this.totalTeams = 0;
-		this.list = [];//ex. 1,2,3,4...,n 
-		this.pairings = [];//ex. [1,2][3,4]...
-		this.showChoices=false; //called 'showChoices' because I wanted to display the teamObjects (for some reason i considered teams as choices..) after the forms have been filled out
-		this.listAllTeams = []; //a list containing all the teams [[TeamObject1, TeamObject2]...etc]
+  app.controller('SetupController', function($scope){
+    this.newTourn = firstBuild;
+	this.hideSetupForm = false;
+	this.showAllTeams = false;
+	//this.totalTeams = 0;
+	this.list = [];//1,2,3,4...,n 
+	this.pairings = [];
+	this.showChoices=false;
+	
+	this.validate = function(pTourn, pTotal){
+	
+		if (isNaN(pTotal)){
+			document.getElementById('errorMessage').innerText = "Please enter a valid number";
+			return false;
+		}
+
+		if ((pTourn == "") || (pTotal == "")){
+			document.getElementById('errorMessage').innerText = "Please fill out all fields";
+			return false;
+		}
 		
-		//Validate function, returning true if the fields are filled ou
-		this.validate = function(pTourn, pTotal){
+		else{
+			document.getElementById('errorMessage').innerText = "";
+			//this.totalTeams = pTotal;
+			this.showAllTeams = true;
+			this.hideSetupForm = true;
+			//create a list of numbers (of the total teams participating)
+			this.buildList(pTotal);
+			//loop through that
+			var finalList = this.test(this.list);
+			return true;
+		}
+	};
+	
+	this.buildList = function(number){
+		for (var i = 0 ; i < number; i++) { 
+			this.list.push(i+1);
+			//var newTeam = new teamObject(i+1);
+			//this.listAllTeams.push(newTeam); //We would hope that by the end of this..
+			//listAllTeams looks like this: [{number: 1}, {number: 2}, {number: 3} ... etc] 
+			//a list of incomplete team info that will be retrieved later..
+		}
+		
+		
+	};
+	
+	$scope.formDataTwo = {};
+	$scope.formDataTwo.list = this.list;
+	
+	//http://stackoverflow.com/questions/12518259/using-ng-repeat-with-table-rows
+	//http://jsfiddle.net/6aqtj/1/
+	
+	this.test = function(listNumbers){
+		this.listAllTeams = []; //to be filled with teamObjects
 
-			if (isNaN(pTotal)){//if the total number of teams is not a number (i.e. user inputs 'seven' instead of 7)
-				document.getElementById('errorMessage').innerText = "Please enter a valid number";
-				return false; //returns false, meaning don't show the other div with the team forms yet
-			}
-
-			if ((pTourn == "") || (pTotal == "")){ //if the set up fields are empty
-				document.getElementById('errorMessage').innerText = "Please fill out all fields";
-				return false; //returns false, meaning don't show the other div with the team forms yet
+		for (var j=0; j < listNumbers.length; j+=2){
+			if (j+1 >= listNumbers.length){
+				this.pairings.push([]);
+				this.pairings[this.pairings.length-1].push(listNumbers[j]);
+				var newTeam = new teamObject(j);
+				this.listAllTeams.push(newTeam);
 			}
 			
 			else{
-				document.getElementById('errorMessage').innerText = ""; //clear the error message div
-				this.showAllTeams = true; //Switches to show the div that will hold all the team forms
-				this.hideSetupForm = true; //hides the set up form
-				//create a list of numbers (of the total teams participating)
-				this.buildList(pTotal); //build the number list [1,2,3... n]
-				this.test(this.list); //pair up the numbers and create team objects for them -- setting 2 different lists. I'm realizing this makes no sense and is doing repetitive work so I'll fix it.
-				return true;
-			}
-		};
-		
-		//buildList is a helper function -- it is never called by the controller on the html side
-		//arguments: number of teams
-		//output: n/a
-		//Populates the list variable to [1,2,3... n]
-		this.buildList = function(number){
-			for (var i = 0 ; i < number; i++) { 
-				this.list.push(i+1);
-			}
-		};
-		
-		//$scope.formDataTwo = {}; 
-		//$scope.formDataTwo.list = this.list;
-
-		//test is a testing function -- never called by the controller on the html side
-		//arguments: list of numbers, that is [1,2,3... n]
-		//output: n/a
-		//Sets the list of teamObject lists (listAllTeams), and sets the pairings list.
-		//Really inefficient and redundant code. Will fix.
-		this.test = function(listNumbers){
-			for (var j=0; j < listNumbers.length; j+=2){ //for every 2 objects in the array starting from 0, 2, 4, etc.
-				if (j+1 >= listNumbers.length){ //if the odd value is the last one in the list (meaning there is no other team to go up against)
-					this.pairings[this.pairings.length-1].push(listNumbers[j]);	//add the odd team to its own list
-					
-					//Concurrently building a list of empty team objects
-					var newTeam = new teamObject(listNumbers[j]);
-					this.listAllTeams.push([]);
-					this.listAllTeams[this.listAllTeams.length-1].push(newTeam);
-				}
+				this.pairings.push([]);
 				
-				else{ //given the value is even and not the last one in the list
-					this.pairings.push([]); //add a new list to the list
-					this.pairings[this.pairings.length-1].push(listNumbers[j]); //add a new number to the list
-					this.pairings[this.pairings.length-1].push(listNumbers[j+1]); //add a new number to the list
-					
-					//Concurrently building a list of empty team objects
-					var newTeam = new teamObject(listNumbers[j]);
-					var newTeam2 = new teamObject(listNumbers[j+1]);
-					this.listAllTeams.push([]);
-					this.listAllTeams[this.listAllTeams.length-1].push(newTeam);
-					this.listAllTeams[this.listAllTeams.length-1].push(newTeam2);
-				}
+				this.pairings[this.pairings.length-1].push(listNumbers[j]);
+				var newTeam = new teamObject(j);
+				this.listAllTeams.push(newTeam);
+				
+				this.pairings[this.pairings.length-1].push(listNumbers[j+1]);
+				var newTeam2 = new teamObject(j+1);
+				this.listAllTeams.push(newTeam2);
 			}
 		}
+		return this.listAllTeams;
+	}
+	//$scope.chosenPairings = this.pairings;
+	
+	//is it even possible? :( http://stackoverflow.com/questions/12044277/how-to-validate-inputs-dynamically-created-using-ng-repeat-ng-show-angular
+	//http://stackoverflow.com/questions/12044277/how-to-validate-inputs-dynamically-created-using-ng-repeat-ng-show-angular
+	this.testR1 = function(){
+		this.showChoices = true;
+		//$scope.testing = "WHATSUP";
 		
-		//A test function to see if I can access the values in the dynamically created form. 
-		this.testR1 = function(){
-			this.showChoices = true;
-			
-			var teamName = document.getElementById("teamName1").value;
-			alert("Team 1's Name : "+teamName);
-			
-			var teamID = document.getElementById("teamID1").value;
-			alert("Team 1's ID : "+teamID);
-			
-			var elements = document.getElementById("allTeamForms").elements;
+		//for every value in list 1 through n (where value is 1, 2, 3, 4 ... n, etc)
+		//create a teamObject 
+		//loop through each field in the form ?? (can we just get it instead of looping???)
+		//update the teamObject
+		//through the teamObject into a bigger list of teamObjects
+		
+	};
 
-		};
-
-	  });
+  });
   
 })();
