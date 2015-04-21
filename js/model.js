6786776dbfb5a5ba866e6ec6238f27ca42b2b4de
EA <eaverett@macalester.edu>
2015-04-21 17:39:40 -0500

    Side constrained pairing now implemented

--- js/model.js
+++ js/model.js
@@ -1,6 +1,17 @@
 window.tournament = { name: "" , totalTeams: 0, roundNumber: 1 };
 window.pairings = [];
 
+//ThenBy.JS microlibrary is Copyright 2013 Teun Duynstee
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//http://www.apache.org/licenses/LICENSE-2.0
+window.firstBy=(function(){function e(f){f.thenBy=t;return f}function t(y,x){x=this;return e(function(a,b){return x(a,b)||y(a,b)})}return e})();
+window.s = firstBy(function (v1, v2) { return v2.record - v1.record; })
+			.thenBy(function (v1, v2) { return v2.combinedStr - v1.combinedStr ; })
+			.thenBy(function (v1, v2) { return v2.pointDiff - v1.pointDiff ; });			
+
+
 function TeamObject(inputnumber) {
 	this.number = inputnumber, //lets make this the one that is assigned by tabber, and uniqueID as the school's special id
 	this.name= "",
