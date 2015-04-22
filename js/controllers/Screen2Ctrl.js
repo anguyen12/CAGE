c13032afe09db950145850b55834a7c71f8f83db
EA <eaverett@macalester.edu>
2015-04-22 13:45:24 -0500

    Fixed "double teams" bug that happened when browser tab not closed.

--- js/controllers/Screen2Ctrl.js
+++ js/controllers/Screen2Ctrl.js
@@ -4,16 +4,17 @@
 var module = angular.module('tabtracker');
 module.controller('Screen2Ctrl', Screen2Ctrl);
 
-//var tournament = { name: "" , totalTeams: 0, roundNumber: 1 };
-//var listAllTeams = [];
-//var pairings = [];
-
 function Screen2Ctrl($scope, $state){
 
 		$scope.initTour = function(){
+			//clear out old data
+			tournament.name = "";
+			tournament.totalTeams = 0;
+			pairings = [];
+			
 			tournament.name = $('#tourName').val();
 			tournament.totalTeams = $('#totalTeams').val();
-			//localStorage.setItem('tournament', JSON.stringify(tournament));
+			
 			
 			for (var j=0; j < tournament.totalTeams; j+=2) {
 				if (j+1 >= tournament.totalTeams){
